4329ab3fa1ce3e1a7f9a1e8323ccd7763cb031a2
tomileonel <tomileonel18@gmail.com>
2024-07-07 01:30:14 -0300

    anti errores de server

--- app/Components/ReservarCitas/index.js
+++ app/Components/ReservarCitas/index.js
@@ -1,28 +1,29 @@
 "use client"
-
 import React, { useState, useEffect } from "react";
 import styles from './style.module.css';
 import Body from '../Body';
 
 function ReservarCitas() {
-  const [citas, setCitas] = useState(
-    JSON.parse(localStorage.getItem('citas')) || []
-  );
+  const [citas, setCitas] = useState(null);
 
   useEffect(() => {
-    const citasGuardadas = JSON.parse(localStorage.getItem('citas'));
-    if (citasGuardadas) {
-      setCitas(citasGuardadas);
+    if (typeof window !== 'undefined') {
+      const citasGuardadas = JSON.parse(localStorage.getItem('citas'));
+      if (citasGuardadas) {
+        setCitas(citasGuardadas);
+      }
     }
-  }, []); 
+  }, []);
 
   useEffect(() => {
-    localStorage.setItem('citas', JSON.stringify(citas));
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('citas', JSON.stringify(citas));
+    }
   }, [citas]);
 
   return (
     <div className="App">
-      <Body setCitas={setCitas} citas={citas} />
+      <Body setCitas={setCitas} citas={citas || []} />
     </div>
   );
 }
