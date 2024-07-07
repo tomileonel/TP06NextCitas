d843e809f19f0fb23df8ca07394760b6eb6481e9
tomileonel <tomileonel18@gmail.com>
2024-07-06 23:57:23 -0300

    intento de subir a githubpg

--- next-reservas/app/Components/subtitulo/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './style.module.css'; 
-
-const Subtitulo = ({ texto }) => {
-  return <h2 className={styles.subtitulo}>{texto}</h2>;
-}
-
-export default Subtitulo;
--- next-reservas/app/Components/titulo/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './style.module.css';
-
-const Titulo = ({texto}) => {
-  return <h1 className={styles.titulo}>{texto}</h1>;
-}
-
-export default Titulo;
